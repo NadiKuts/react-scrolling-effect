b41976b850eb4db532161e6e66bd04cbbc4841b5
Nadezhda Kutsenko <nadikutsenko@gmail.com>
2017-08-08 23:37:39 +0200

    Identify onScroll event

--- src/components/entree/Entree.js
+++ src/components/entree/Entree.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './Entree.css';
 import SideEntree from './SideEntree';
 import ContainerEntree from './ContainerEntree';
 
 class Entree extends Component {
+  onScroll = () => {
+    
+    if (ReactDOM.findDOMNode(this.refs.entree).getBoundingClientRect().top > 0 && 
+      ReactDOM.findDOMNode(this.refs.entree).getBoundingClientRect().top < window.innerHeight) {
+      console.log('in view!');
+    }
+  }
+  componentDidMount() {
+    window.addEventListener('scroll', this.onScroll.bind(this));
+  }
   render() {
     return (
-      <div className="Entree">
+      <div className="Entree" ref="entree">
         <SideEntree></SideEntree>
         <ContainerEntree></ContainerEntree>
       </div>
