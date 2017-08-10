278fd9590cb9c858b8b2d232a38e72f74cccd2e6
Nadezhda Kutsenko <nadikutsenko@gmail.com>
2017-08-11 00:05:06 +0200

    Change opacity of EntreeCard onload

--- src/components/entree/Entree.js
+++ src/components/entree/Entree.js
@@ -9,28 +9,49 @@ class Entree extends Component {
   constructor(props) {
 		super(props);
 		this.state = {
-			isScrolled: false
+			isScrolled: false,
+      wasLoaded: false,
 		};
     this.onScroll = this.onScroll.bind(this);
 	}
+  
   onScroll = () => {
     if (this.state.isScrolled == false) {
       if (ReactDOM.findDOMNode(this.refs.entree).getBoundingClientRect().top > 0 && 
         ReactDOM.findDOMNode(this.refs.entree).getBoundingClientRect().top < window.innerHeight) {
           	this.setState({isScrolled: true});
+      } else {
       }
     } else {
     }
   }
   componentDidMount() {
     window.addEventListener('scroll', () => this.onScroll());
+    this.setState({wasLoaded: !this.state.wasLoaded});
   }
   render() {
     return (
-      <div className="Entree" ref="entree">
-        <SideEntree scrolled={this.state.isScrolled}></SideEntree>
-        <ContainerEntree scrolled={this.state.isScrolled}></ContainerEntree>
-      </div>
+      <Motion ref="entree" style={{ 
+
+          opacity: spring(this.state.wasLoaded ? 1 : 0),
+         }}>
+         {({width, opacity}) =>
+         <div className="Entree" style={{
+            
+                opacity: `${opacity}`,
+                WebkitTransitionDuration: '0.5s',
+                transitionDuration: '0.5s',
+                WebkitTransitionTimingFunction: 'ease',
+                transitionTimingFunction: 'ease',
+                WebkitTransitionDelay: '1s',
+                transitionDelay: '1s',
+              }}>
+           <SideEntree scrolled={this.state.isScrolled}></SideEntree>
+           <ContainerEntree scrolled={this.state.isScrolled}></ContainerEntree>
+         </div>
+       }
+      </Motion>
+      
     );
   }
 }
