1586fa8c4ff9f3870b34b66c635d25172357eb0b
Nadezhda Kutsenko <nadikutsenko@gmail.com>
2017-08-13 19:08:14 +0200

    Detect component in view

--- src/components/entree/Entree.js
+++ src/components/entree/Entree.js
@@ -9,25 +9,77 @@ class Entree extends Component {
   constructor(props) {
 		super(props);
 		this.state = {
-			isScrolled: false,
+			isScrolledFirst: false,
+      initialAnimation: false,
+      inView: false,
       wasLoaded: false,
+      lastScrollPos: 0,
+      direction: '',
+      absTopPos: 0,
 		};
     this.onScroll = this.onScroll.bind(this);
 	}
   
-  onScroll = () => {
-    if (this.state.isScrolled == false) {
-      if (ReactDOM.findDOMNode(this.refs.entree).getBoundingClientRect().top > 0 && 
-        ReactDOM.findDOMNode(this.refs.entree).getBoundingClientRect().top < window.innerHeight) {
-          	this.setState({isScrolled: true});
-      } else {
-      }
+  ifVisible = (bPart, tPart, wHeight) => {
+    if ((bPart < window.scrollY + wHeight) && (tPart > 0 && tPart < wHeight)) {
+      this.setState({
+        inView: true,
+      });
     } else {
+      this.setState({
+        inView: false,
+      });
     }
   }
+  
+  onScroll = (event) => {
+    const topPart = ReactDOM.findDOMNode(this.refs.entree).getBoundingClientRect().top;
+    const bottomPart = ReactDOM.findDOMNode(this.refs.entree).getBoundingClientRect().bottom;
+    const winHeight = window.innerHeight;
+    /** Detect direction of scrolling */
+    if (this.state.lastScrollPos < window.scrollY) {
+      this.setState({
+        lastScrollPos: window.scrollY,
+        direction: 'down',
+      });
+    } else {
+      this.setState({
+        lastScrollPos: window.scrollY,
+        direction: 'up',
+      });
+    }
+    /** Detect if component is completely visible */
+    if (this.state.isScrolledFirst == false) {
+      this.setState({
+        isScrolledFirst: true,
+        initialAnimation: true,
+      });
+      this.ifVisible(bottomPart, topPart, winHeight);
+
+    } else {
+      this.setState({
+        initialAnimation: false,
+      });
+      this.ifVisible(bottomPart, topPart, winHeight);
+    }
+  }
+      
+      
+    //   if (ReactDOM.findDOMNode(this.refs.entree).getBoundingClientRect().top > 0 && 
+    //     ReactDOM.findDOMNode(this.refs.entree).getBoundingClientRect().top < window.innerHeight) {
+    //       	this.setState({inView: true});
+    //         console.log('scrolled');
+    //   } else {
+    //   }
+    // } else {
+    // }
+  
   componentDidMount() {
-    window.addEventListener('scroll', () => this.onScroll());
-    this.setState({wasLoaded: !this.state.wasLoaded});
+    window.addEventListener('scroll', (event) => this.onScroll(event));
+    this.setState({
+      wasLoaded: !this.state.wasLoaded,
+      absTopPos: parseInt((ReactDOM.findDOMNode(this.refs.entree).getBoundingClientRect().top - document.body.getBoundingClientRect().top), 10),
+    });
   }
   render() {
     return (
@@ -46,8 +98,8 @@ class Entree extends Component {
                 WebkitTransitionDelay: '1s',
                 transitionDelay: '1s',
               }}>
-           <SideEntree scrolled={this.state.isScrolled}></SideEntree>
-           <ContainerEntree scrolled={this.state.isScrolled}></ContainerEntree>
+           <SideEntree scrolled={this.state.inView} firstTime={this.initialAnimation}></SideEntree>
+           <ContainerEntree scrolled={this.state.inView} firstTime={this.initialAnimation}></ContainerEntree>
          </div>
        }
       </Motion>
